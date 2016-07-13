c82e9bb24d8735a75f1c372c5accc96d0353fee4
tjorriemorrie <jacoj82@gmail.com>
2016-07-13 14:36:48 +0200

    webpack chunking set up

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -2,18 +2,19 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'react/jsx/index.jsx'),
+    entry: path.resolve(__dirname, 'frontend/index.js'),
     output: {
-        path: path.resolve(__dirname, 'static/build'),
+        path: path.resolve(__dirname, 'backend/static/build'),
         publicPath: 'http://localhost:9898/static/build',
-        filename: 'pokeraide.js'
+        filename: '[name].js',
+        chunkFilename: '[id].chunk.js'
     },
     module: {
         loaders: [
             {
                 test: /\.jsx?$/,
                 exclude: /(node_modules|bower_components)/,
-                loaders: ['react-hot', 'babel?cacheDirectory,presets[]=react,presets[]=es2015']
+                loaders: ['react-hot', 'babel?cacheDirectory']
             },
             {
                 test: /\.less$/,
@@ -24,5 +25,8 @@ module.exports = {
                 loader: 'style!css'
             }
         ]
-    }
+    },
+    plugins: [
+         new webpack.optimize.CommonsChunkPlugin({name:'commons'})
+    ]
 };
